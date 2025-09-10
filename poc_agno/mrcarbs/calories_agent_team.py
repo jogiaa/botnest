989158adb433278098790334fa0e9029d9142c7c
Asim <jogiaa@gmail.com>
2025-09-09 23:24:09 -0400

    Updating team's instructions

--- poc_agno/mrcarbs/calories_agent_team.py
+++ poc_agno/mrcarbs/calories_agent_team.py
@@ -1,31 +1,32 @@
-from pprint import pprint
-
 from agno.team import Team
 from pydantic import BaseModel
 
-from poc_agno.agents.simplest_math_agent import math_agent
-from poc_agno.agents.web_search_ddg_agent import web_agent
 from poc_agno.llm_model_config import llm_model
+from poc_agno.mrcarbs.agent.food_search.food_search_agent import food_search_agent
+from poc_agno.mrcarbs.agent.recipie_search.recipie_search_agent import recipe_aggregator_agent
 from poc_agno.utils.load_instructions import load_yaml_instructions
 
 
-
-class CarbInfo(BaseModel):
-    ingredients: str
-    ingredient_carbs: str
-
 class DishCarbInfo(BaseModel):
     dish: str
-    ingredients: list[CarbInfo]
     carbs: float
 
 
 agent_team = Team(
+    name="003_CarbPerServingCalculator",
+    role="""
+    The CarbPerServingCalculator agent computes the carbohydrate content per serving of a 
+    dish by pulling recipe data from 002_RecipeAggregator, looking up ingredient 
+    carb values with 001_food_search, and returning the result in a concise JSON format.
+    """,
+    description="""
+    Orchestrates carb calculations for a dish by consuming structured recipe data from 
+    002_RecipeAggregator, querying 001_food_search for ingredient carbs, and returning 
+    the carb amount per serving in a clean JSON format.""",
     mode="coordinate",
-    members=[web_agent, math_agent],
+    members=[recipe_aggregator_agent, food_search_agent],
     model=llm_model,
-    description="Search recipes and estimate carbs content per serving.",
-    success_criteria="Find accurate or estimated carbs count using web search and calculations.",
+    success_criteria="Accurately determine or estimate the carbohydrate content of a dish by retrieving data through given team members and web",
     instructions=load_yaml_instructions("instructions.yaml"),
     response_model=DishCarbInfo,
     show_tool_calls=True,
@@ -35,4 +36,4 @@ agent_team = Team(
 )
 
 if __name__ == "__main__":
-    agent_team.print_response("Find carbs of one bowl of Mutton Haleem?", stream=True)
+    agent_team.print_response("Find carbs of one bowl of calm chowder?", stream=True)
